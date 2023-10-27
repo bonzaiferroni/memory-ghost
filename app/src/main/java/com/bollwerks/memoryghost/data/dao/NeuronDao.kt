6d0b1fa8feb32dcc94668ecf899931c31b6cc929
Luke Bollwerk <lukas8@gmail.com>
2023-10-27 14:45:59 -0600

    add dao

--- app/src/main/java/com/bollwerks/memoryghost/data/dao/NeuronDao.kt
+++ app/src/main/java/com/bollwerks/memoryghost/data/dao/NeuronDao.kt
@@ -1,4 +1,31 @@
 package com.bollwerks.memoryghost.data.dao
 
-class NeuronDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.bollwerks.memoryghost.model.Neuron
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NeuronDao {
+    @Query("SELECT * FROM neuron")
+    fun getAll(): Flow<List<Neuron>>
+
+    @Query("SELECT * FROM neuron WHERE id = :id")
+    fun getById(id: Int): Flow<Neuron>
+
+    @Query("SELECT * FROM neuron WHERE name LIKE :name")
+    fun getByName(name: String): Flow<List<Neuron>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(neuron: Neuron)
+
+    @Update
+    suspend fun update(neuron: Neuron)
+
+    @Delete
+    suspend fun delete(neuron: Neuron)
 }
