633937a6e63536d6144a17e5fc813b95502aef85
Luke Bollwerk <lukas8@gmail.com>
2023-11-07 09:16:36 -0700

    refactor vm provision

--- app/src/main/java/com/bollwerks/memoryghost/AppConfig.kt
+++ app/src/main/java/com/bollwerks/memoryghost/AppConfig.kt
@@ -1,13 +1,75 @@
 package com.bollwerks.memoryghost
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Text
 import androidx.compose.ui.res.painterResource
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.bollwerks.eznav.DrawerConfig
+import com.bollwerks.eznav.DrawerItemConfig
 import com.bollwerks.eznav.EzConfig
+import com.bollwerks.eznav.NavComposableConfig
+import com.bollwerks.eznav.NavHostConfig
+import com.bollwerks.memoryghost.data.AppDatabase
+import com.bollwerks.memoryghost.data.DaoRepository
+import com.bollwerks.memoryghost.ui.neuron.NeuronTreeModel
+import com.bollwerks.memoryghost.ui.neuron.NeuronTreeScreen
+import com.bollwerks.memoryghost.ui.sandbox.SandboxScreen
 
 val appConfig = EzConfig(
 
     drawerConfig = DrawerConfig(
-        defaultRoute = AppRoutes.Neuron,
         mainAppIcon = { painterResource(R.drawable.ic_launcher_foreground) },
-    )
+        drawerItems = listOf(
+            DrawerItemConfig(AppRoutes.Home, "Home", Icons.Filled.Home),
+            DrawerItemConfig(AppRoutes.Neuron, "Neuron", Icons.Filled.Face),
+            DrawerItemConfig(AppRoutes.Sandbox, "Sandbox", Icons.Filled.Build),
+        )
+    ),
+
+    navHostConfig = NavHostConfig(
+        initialRoute = AppRoutes.Neuron,
+        vmFactoryBuilder = { ctx ->
+            val appDatabase = AppDatabase.getDatabase(ctx)
+            val dataRepository = DaoRepository(appDatabase.neuronDao())
+
+            viewModelFactory {
+                initializer {
+                    val savedStateHandle = createSavedStateHandle()
+                    NeuronTreeModel(savedStateHandle, dataRepository)
+                }
+            }
+        },
+        composableConfigs = listOf(
+            NavComposableConfig(
+                route = AppRoutes.Home.route,
+                content = { _, _, _ ->
+                    Text("Hello home!")
+                }
+            ),
+            NavComposableConfig(
+                route = AppRoutes.Neuron.route,
+                content = { navController, drawerState, vmFactory ->
+                    NeuronTreeScreen(
+                        drawerState = drawerState,
+                        navController = navController,
+                        viewModel = viewModel(factory = vmFactory),
+                    )
+                }
+            ),
+            NavComposableConfig(
+                route = AppRoutes.Sandbox.route,
+                content = { _, drawerState, _ ->
+                    SandboxScreen(
+                        drawerState = drawerState,
+                    )
+                }
+            ),
+        )
+    ),
 )
--- app/src/main/java/com/bollwerks/memoryghost/MainActivity.kt
+++ app/src/main/java/com/bollwerks/memoryghost/MainActivity.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Build
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.bollwerks.eznav.AppDestination
 import com.bollwerks.eznav.EzNav
 import com.bollwerks.eznav.NavComposableConfig
 import com.bollwerks.eznav.NavHostConfig
@@ -28,46 +23,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val appDatabase = remember { AppDatabase.getDatabase(applicationContext) }
-            val daoRepository = remember { DaoRepository(appDatabase.neuronDao()) }
-            val vmProvider = remember { ViewModelProvider(daoRepository) }
-            val destinations = arrayListOf(
-                AppDestination(AppRoutes.Home, "Home", Icons.Filled.Home),
-                AppDestination(AppRoutes.Neuron, "Neuron", Icons.Filled.Face),
-                AppDestination(AppRoutes.Sandbox, "Sandbox", Icons.Filled.Build),
-            )
-
-            val navHostConfig = remember {
-                NavHostConfig(
-                    startDestination = AppRoutes.Neuron.route,
-                    composableConfigs = listOf(
-                        NavComposableConfig(
-                            route = AppRoutes.Home.route,
-                            content = { _, _ ->
-                                Text("Hello home!")
-                            }
-                        ),
-                        NavComposableConfig(
-                            route = AppRoutes.Neuron.route,
-                            content = { navController, drawerState ->
-                                NeuronTreeScreen(
-                                    drawerState = drawerState,
-                                    navController = navController,
-                                    viewModel = viewModel(factory = vmProvider.Factory),
-                                )
-                            }
-                        ),
-                        NavComposableConfig(
-                            route = AppRoutes.Sandbox.route,
-                            content = { _, drawerState ->
-                                SandboxScreen(
-                                    drawerState = drawerState,
-                                )
-                            }
-                        ),
-                    )
-                )
-            }
 
             MemoryGhostTheme {
                 // A surface container using the 'background' color from the theme
@@ -77,8 +32,6 @@ class MainActivity : ComponentActivity() {
                 ) {
                     EzNav(
                         config = appConfig,
-                        destinations = destinations,
-                        navHostConfig = navHostConfig,
                     )
                 }
             }
