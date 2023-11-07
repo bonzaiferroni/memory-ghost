97f0e496d5b5981bb797b85a398c24e136e24d3d
Luke Bollwerk <lukas8@gmail.com>
2023-11-07 10:03:09 -0700

    refactor EzConfig

--- app/src/main/java/com/bollwerks/memoryghost/AppConfig.kt
+++ app/src/main/java/com/bollwerks/memoryghost/AppConfig.kt
@@ -10,10 +10,8 @@ import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.bollwerks.eznav.model.DrawerConfig
-import com.bollwerks.eznav.model.DrawerItemConfig
+import com.bollwerks.eznav.model.DrawerLinkConfig
 import com.bollwerks.eznav.model.EzConfig
-import com.bollwerks.eznav.model.NavHostConfig
 import com.bollwerks.eznav.model.ScreenConfig
 import com.bollwerks.memoryghost.data.AppDatabase
 import com.bollwerks.memoryghost.data.DaoRepository
@@ -23,53 +21,47 @@ import com.bollwerks.memoryghost.ui.sandbox.SandboxScreen
 
 val appConfig = EzConfig(
 
-    drawerConfig = DrawerConfig(
-        mainAppIcon = { painterResource(R.drawable.ic_launcher_foreground) },
-        drawerItems = listOf(
-            DrawerItemConfig(AppRoutes.Home, "Home", Icons.Filled.Home),
-            DrawerItemConfig(AppRoutes.Neuron, "Neuron", Icons.Filled.Face),
-            DrawerItemConfig(AppRoutes.Sandbox, "Sandbox", Icons.Filled.Build),
-        )
-    ),
+    mainAppIcon = { painterResource(R.drawable.ic_launcher_foreground) },
 
-    navHostConfig = NavHostConfig(
-        initialRoute = AppRoutes.Neuron,
-        vmFactoryBuilder = { ctx ->
-            val appDatabase = AppDatabase.getDatabase(ctx)
-            val dataRepository = DaoRepository(appDatabase.neuronDao())
+    vmFactoryBuilder = { ctx ->
+        val appDatabase = AppDatabase.getDatabase(ctx)
+        val dataRepository = DaoRepository(appDatabase.neuronDao())
 
-            viewModelFactory {
-                initializer {
-                    val savedStateHandle = createSavedStateHandle()
-                    NeuronTreeModel(savedStateHandle, dataRepository)
-                }
+        viewModelFactory {
+            initializer {
+                val savedStateHandle = createSavedStateHandle()
+                NeuronTreeModel(savedStateHandle, dataRepository)
             }
-        },
-        screenConfigs = listOf(
-            ScreenConfig(
-                route = AppRoutes.Home.route,
-                content = { _, _, _ ->
-                    Text("Hello home!")
-                }
-            ),
-            ScreenConfig(
-                route = AppRoutes.Neuron.route,
-                content = { navController, drawerState, vmFactory ->
-                    NeuronTreeScreen(
-                        drawerState = drawerState,
-                        navController = navController,
-                        viewModel = viewModel(factory = vmFactory),
-                    )
-                }
-            ),
-            ScreenConfig(
-                route = AppRoutes.Sandbox.route,
-                content = { _, drawerState, _ ->
-                    SandboxScreen(
-                        drawerState = drawerState,
-                    )
-                }
-            ),
-        )
-    ),
+        }
+    },
+    screens = listOf(
+        ScreenConfig(
+            route = AppRoutes.Home,
+            drawerLink = DrawerLinkConfig(AppRoutes.Home, "Home", Icons.Filled.Home),
+            content = { _, _, _ ->
+                Text("Hello home!")
+            }
+        ),
+        ScreenConfig(
+            route = AppRoutes.Neuron,
+            isDefaultRoute = true,
+            drawerLink = DrawerLinkConfig(AppRoutes.Neuron, "Neuron", Icons.Filled.Face),
+            content = { navController, drawerState, vmFactory ->
+                NeuronTreeScreen(
+                    drawerState = drawerState,
+                    navController = navController,
+                    viewModel = viewModel(factory = vmFactory),
+                )
+            }
+        ),
+        ScreenConfig(
+            route = AppRoutes.Sandbox,
+            drawerLink = DrawerLinkConfig(AppRoutes.Sandbox, "Sandbox", Icons.Filled.Build),
+            content = { _, drawerState, _ ->
+                SandboxScreen(
+                    drawerState = drawerState,
+                )
+            }
+        ),
+    )
 )
